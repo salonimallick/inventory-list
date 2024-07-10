b64f943129e1fefb6199b8cbfe3fefdfd9cc0929
saloni mallick <salonimallick026@gmail.com>
2024-07-10 19:20:36 +0530

    Inventory List

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-
+import InventoryList from "./components/inventoryList"
 
 function App() {
 
   return (
     <>
-      <h1
-        className="text-3xl text-center text-red-700"
-      >Welcome to Vite with TailwindCSS and React</h1>
+      <InventoryList />
     </>
   )
 }
---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+const FETCH_ACTIONS = {
+    PROGRESS: 'purpose',
+    SUCCESS: 'success',
+    ERROR: 'error',
+}
+
+export{FETCH_ACTIONS};
